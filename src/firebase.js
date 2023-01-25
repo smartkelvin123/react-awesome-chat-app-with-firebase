27ebc4ac4de4bf0e2df5271ce41d239d26d85205
smartkelvin123 <105611054+smartkelvin123@users.noreply.github.com>
2023-01-25 22:00:49 +0100

    update file on firebase

--- src/firebase.js
+++ src/firebase.js
@@ -10,15 +10,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB29zRd9o1mUesEl7_VIlATQqqiVgaZjm0",
-  authDomain: "chat-app-9b24c.firebaseapp.com",
-  projectId: "chat-app-9b24c",
-  storageBucket: "chat-app-9b24c.appspot.com",
-  messagingSenderId: "1011782091008",
-  appId: "1:1011782091008:web:371c9bd1c46a71baeaf3e5",
-  measurementId: "G-J52QL55DJ9",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
